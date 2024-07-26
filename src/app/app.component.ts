2eb749309cd145513cb35311ed68059f07e36290
Kamil Abzalov <mail@kamil-abzalov.com>
2024-07-26 22:31:54 +0300

    Make basic app structure, add prettier and eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'cloudonix-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'cloudonix-assessment';
-}
+export class AppComponent {}
